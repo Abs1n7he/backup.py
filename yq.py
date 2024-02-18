713b14031933ff3db03928d654627af2b0fd6a1d
Abs1n7he <wuxinyangliu@163.com>
2024-02-18 20:27:50 +0800

    20240218

---
+++ yq.py
@@ -0,0 +1,270 @@
+import sys
+import re
+import os
+import json
+import time
+import requests
+import urllib
+import urllib3
+urllib3.disable_warnings()
+from PyQt5.QtGui import QIcon,QTextCharFormat
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
+
+proxies={}
+def ecd(str):
+    return str.replace('<','&lt;').replace('>','&gt;')
+def str_to_json(str):
+    dict={}
+    for i in str.split('\n'):
+        dict[i.partition(':')[0]]=i.partition(':')[2].strip()
+    while '' in dict.keys():
+        dict.pop('')
+    return dict
+
+def json_to_str(dict):
+    return ''.join([key+': '+value+'\n' for key,value in dict.items()])
+
+import xml.etree.ElementTree as ET
+def printXML(str):          # XML 字符串格式化打印
+    # 创建XML元素
+    element = ET.XML(str)
+    # 使用indent()函数进行格式化打印
+    ET.indent(element)
+    return ET.tostring(element, encoding='unicode')
+
+def run(session):
+    ############## 设置代理 ##############
+    if proxy_button.isChecked():
+        proxies.update({'http':proxy.text(),'https':proxy.text()})
+
+    ############## 从request获取请求方式、path、header等 ##############
+    req=requests1.toPlainText()
+    method= req.partition(' ')[0].strip().lower()
+    path=   req.partition(' ')[2].partition(' ')[0].strip()
+    host=   req.partition('\n')[2].partition('\n')[0].partition(':')[2].strip()
+    headers=req.partition('\n')[2].partition('\n')[2].partition('\n\n')[0].strip()
+    if not (method and path and host):#缺失告警
+        response.setText('<span style="color:#ff0000">bad request</span>')
+        return
+    # print(method)
+    # print(path)
+    # print(host)
+    # print(headers)
+
+    ############## 删除长度，替换session ##############
+    dict_headers=str_to_json(headers)
+    try:dict_headers.pop('Content-length')
+    except:pass
+    for key,value in str_to_json(session).items():
+        dict_headers[key]=value
+
+    ############## 删除指定请求头 ##############
+    if delete_header_button.isChecked():
+        for i in delete_req_header.toPlainText().split(','):
+            try:dict_headers.pop(i.strip())
+            except:pass
+
+    ############## 发送请求 ##############
+    url = https.currentText() + "://" + host + path
+    if method in ['get', 'options', 'delete']:
+        try:
+            res = eval('requests.' + method + '(url,headers=dict_headers,timeout=20,verify=False,proxies=proxies,allow_redirects=False)')
+        except:#请求失败告警
+            response.setText('<span style="color:#ff0000">request fail,check host or http</span>')
+            return
+
+    elif method in ['post', 'put']:
+        try:
+            body = req.partition('\n\n')[2]
+            res = eval('requests.' + method + '(url,headers=dict_headers,data=body,timeout=20,verify=False,proxies=proxies,allow_redirects=False)')
+        except:#请求失败告警
+            response.setText('<span style="color:#ff0000">request fail,check host or http</span>')
+            return
+    else:#请求方式错误告警
+        response.setText('<span style="color:#ff0000">bad method</span>')
+        return
+    # print(res.status_code)
+
+
+    ############## 记录日志 ##############html
+    if LogSwitch=='True':
+        with open(logFile,'a',encoding='utf-8') as log:
+            log.write(time.strftime("[request %Y %m %d %H:%M:%S]\n",time.localtime(time.time()))+req+'\n\n')
+            log.write('[response]\n'+str(res.status_code)+" "+res.reason+"\n"+
+                      ''.join([k+':'+v+'\n' for k,v in res.headers.items()])+'\n'+
+                      res.text+'\n\n')
+
+    ############## 检查响应头 ##############
+    bad_res_header = []  # 错误响应头
+    lack_res_header = []  # 缺失响应头
+    if check_res_header_button.isChecked():
+        for key,value in str_to_json(check_res_header.toPlainText()).items():
+            if key in res.headers.keys() and not(value==res.headers[key] or re.findall(value,res.headers[key])):
+                bad_res_header.append(key)
+            else:
+                lack_res_header.append(key)
+
+    ############## 输出响应码，响应头 ##############
+    response_text=str(res.status_code)+' '+res.reason+'<br>'
+    for key,value in res.headers.items():
+        if key in bad_res_header:
+            response_text+='<span style="color:#ff0000">%s: %s</span><br>'% (str(key),str(value))
+        else:
+            response_text += '<span style="color:#00a000">%s:</span> %s<br>' % (str(key), str(value))
+    if len(lack_res_header)>0:
+        response_text+='<span style="color:#ff0000">缺失响应头: <br>'+'<br>'.join(lack_res_header)+'</span><br>'
+    response.setText(response_text)
+
+    ############## 输出响应体 ##############
+    if 'application/json' in res.headers['Content-Type'] and res.text[0]=='{':
+        response.append(json.dumps(json.loads(res.text),indent=4,ensure_ascii=False,sort_keys=False,separators=(',',';')))
+    elif 'xml'in res.headers['Content-Type'] and res.text[0]=='<':
+        response.append(printXML(res.text).replace('ns0:',''))
+    else:
+        response.append('<br>'+ecd(res.text))
+
+
+configFile='BlackBox.config'
+logFile=time.strftime("%Y%m%d.log",time.localtime(time.time()))
+if not os.path.isfile(configFile):
+    with open(configFile,'w',encoding='utf-8') as f:
+        f.write('#日志记录\nLog=False\n\n'+
+                '#http代理\nproxy=http://127.0.0.1:8080\n\n'+
+                '用户session\n'+
+                'session5={"Referer":"baidu.com","origin":""}\n'+
+                'session6={"Cookie":"","X-Csrf_Token":""}\n\n'+
+                '#删除指定请求头\n'+
+                'delete_req_header=Cookie,X-Csrf_Token,Referer\n\n'+
+                '#检查响应头\n'+
+                'cherk_res_header={"x-content-type-option":"nosniff","Access-Control-Allow-Origin":"^http(.*?).baidu.com","Access-Control-Allow-Credentials":"true","X-Frame-Options":"(SAMEORIGIN)|(sameorigin)","Content-Security-Policy":"frame-ancestors https://*.baidu.com","x-xss-protection":"1; mode=block"}')
+        f.close()
+
+f = open(configFile,'r',encoding='utf-8')
+sessions={}
+LogSwitch='False'
+for line in f:
+    if line.startswith('Log'):
+        LogSwitch=line.partition('=')[2].strip()
+    elif line.startswith('proxy'):
+        proxy1=line.partition('=')[2].strip()
+    elif line.startswith('session'):
+        sessions[line.partition('=')[0].strip()]=line.partition('=')[2].strip()
+    elif line.startswith('delete_req_header'):
+        delete_req_header1=line.partition('=')[2].strip()
+    elif line.startswith('cherk_res_header'):
+        check_res_header1=line.partition('=')[2].strip()
+
+app=QApplication(sys.argv)
+
+
+w=QWidget()
+w.setWindowTitle("黑盒越权测试 Bate   By:樊宜沛")
+w.resize(int(app.desktop().width()*0.9),int(app.desktop().height()*0.8))
+w.setWindowIcon(QIcon('logo.ico'))
+
+############## 1 ##############
+grid=QGridLayout()
+https=QComboBox()
+https.addItems(['https','http'])
+grid.addWidget(https,0,0)
+grid.addWidget(QLabel('session1'),1,0)
+grid.addWidget(QLabel('session2'),2,0)
+grid.addWidget(QLabel('session3'),3,0)
+grid.addWidget(QLabel('session4'),4,0)
+grid.addWidget(QLabel('session5'),5,0)
+grid.addWidget(QLabel('session6'),6,0,2,1)
+
+############## 2 ##############
+session1=QTextEdit()
+
+session2=QTextEdit()
+session3=QTextEdit()
+session4=QTextEdit()
+session5=QTextEdit()
+session6=QTextEdit()
+grid.addWidget(session1,1,1)
+grid.addWidget(session2,2,1)
+grid.addWidget(session3,3,1)
+grid.addWidget(session4,4,1)
+grid.addWidget(session5,5,1)
+grid.addWidget(session6,6,1,2,1)
+for key,value in sessions.items():
+    eval(key+".setText(json_to_str(json.loads(sessions['"+key+"'])).strip())")
+
+############## 3 ##############
+proxy_button=QCheckBox("启用代理")
+grid.addWidget(proxy_button,0,2)
+run1=QPushButton("run")
+run2=QPushButton("run")
+run3=QPushButton("run")
+run4=QPushButton("run")
+run5=QPushButton("run")
+run6=QPushButton("run")
+run1.clicked.connect(lambda :run(session1.toPlainText()))
+run2.clicked.connect(lambda :run(session2.toPlainText()))
+run3.clicked.connect(lambda :run(session3.toPlainText()))
+run4.clicked.connect(lambda :run(session4.toPlainText()))
+run5.clicked.connect(lambda :run(session5.toPlainText()))
+run6.clicked.connect(lambda :run(session6.toPlainText()))
+grid.addWidget(run1,1,2)
+grid.addWidget(run2,2,2)
+grid.addWidget(run3,3,2)
+grid.addWidget(run4,4,2)
+grid.addWidget(run5,5,2)
+grid.addWidget(run6,6,2,2,1)
+
+############## 4 ##############
+proxy=QLineEdit()
+try:proxy.setText(proxy1.strip())
+except:pass
+requests1=QTextEdit()
+requests1.setPlaceholderText("request")
+
+delete_header_button=QCheckBox("删除指定请求头")
+delete_req_header=QTextEdit()
+try:delete_req_header.setText(delete_req_header1.strip())
+except:pass
+
+grid.addWidget(proxy,0,3)
+grid.addWidget(requests1,1,3,5,1)
+grid.addWidget(delete_header_button,6,3)
+grid.addWidget(delete_req_header,7,3)
+
+############## 5 ##############
+
+response=QTextEdit()
+response.setPlaceholderText("response")
+check_res_header_button=QCheckBox("检查响应头")
+check_res_header=QTextEdit()
+try:check_res_header.setText(json_to_str(json.loads(check_res_header1)).strip())
+except:pass
+
+grid.addWidget(response,1,4,5,1)
+grid.addWidget(check_res_header_button,6,4)
+grid.addWidget(check_res_header,7,4)
+
+
+############## 不接受用户的富文本插入 ##############
+session1.setAcceptRichText(False)
+session2.setAcceptRichText(False)
+session3.setAcceptRichText(False)
+session4.setAcceptRichText(False)
+session5.setAcceptRichText(False)
+session6.setAcceptRichText(False)
+check_res_header.setAcceptRichText(False)
+response.setAcceptRichText(False)
+delete_req_header.setAcceptRichText(False)
+requests1.setAcceptRichText(False)
+
+
+
+w.setLayout(grid)
+w.show()
+app.exec()
+
+
+
+
+
+
